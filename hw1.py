908e8d80b23bb9a882f412954e055b77c6ae47a4
Aliman Bibolov <jorgefibonacci413@gmail.com>
2025-01-19 00:00:56 +0500

    Added variable and used if-else operations

--- hw1.py
+++ hw1.py
@@ -6,3 +6,13 @@ print(sys.version)
 # here 1st assignment  
 print("Hello world")
 
+#here example using variable and input from terminal
+n = int(input())
+
+#here another example of using if
+if n > 2:
+    print("No there other people here")
+if n == 2:
+    print("Just the two of us")
+
+
